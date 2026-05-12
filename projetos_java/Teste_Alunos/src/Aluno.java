3a5cb36a8570bd8ce007c538a0f19a901b950d16
Anderson <andysantospupomuniz@gmail.com>
2026-05-12 20:34:23 -0300

    finalização da classe aluno
    Added approval message for failed students and a method to generate a report.

--- projetos_java/Teste_Alunos/src/Aluno.java
+++ projetos_java/Teste_Alunos/src/Aluno.java
@@ -7,11 +7,16 @@ public class Aluno{
     public double Media(){
         return (nota1 + nota2) / 2;
     }
-    public void aprovado(){
+    public void Aprovado(){
         if (Media() >= 6.0){
-            System.out.println("Aluno"+nome+"aprovado");
+            System.out.println("Aluno "+nome+" aprovado");
+        }
+        else{
+            Sysem.out.println("Aluno "+nome+" Reprovado")
         }
     }
-    
+    public void Boletim(){
+        return "Nome: "+nome+" Matricula: "+matricula+" nota 1: "+nota1+" nota 2: "+nota2+" sua Média é: "+Media+" Está: "+Aprovado;
+    }
 
 }
