455bb9332e5bbd3a23f5fce067392561b0ed3cd0
FuzzyCultist <andysantospupomuniz@gmail.com>
2026-05-13 09:20:31 -0300

    att alunos

--- projetos_java/Teste_Alunos/src/App.java
+++ projetos_java/Teste_Alunos/src/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        System.out.println("-------------------------------------------------");
 
         Aluno aluno1 = new Aluno();
         aluno1.nome = "maria";
@@ -15,6 +16,7 @@ public class App {
         aluno2.nota2 = 12.90;
 
         System.out.println(aluno1.Boletim());
+        System.out.println("-------------------------------------------------");
         System.out.println(aluno2.Boletim());
 
     }
